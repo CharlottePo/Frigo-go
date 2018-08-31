a09f80f1808188dc1393c736c68768be5a5f746f
guinardpaul <guinardpaul@gmail.com>
2018-08-31 21:15:22 +0200

    init project

---
+++ src/main/java/fr/chapaul/frigogo/App.java
@@ -0,0 +1,13 @@
+package fr.chapaul.frigogo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
